ce0b006611a59713c701ba24c2f88a47277b0770
BHANUPRAKASHTHOTA1 <bhanuprakash.thota546@gmail.com>
2021-02-19 12:09:19 +0530

    first commit

---
+++ src/test/java/qaacademy/maven2java/RestApiTest.java
@@ -0,0 +1,16 @@
+package qaacademy.maven2java;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	@Test
+	public void RestAPi()
+	{
+		System.out.println("RestApi");
+	}
+	@Test
+	public void RsetApi2()
+	{
+		System.out.println("RestApi2");
+	}
+}
---
+++ src/test/java/qaacademy/maven2java/seleniumTest.java
@@ -0,0 +1,17 @@
+package qaacademy.maven2java;
+
+import org.testng.annotations.Test;
+
+public class seleniumTest {
+
+	@Test
+	public void webapp()
+	{
+		System.out.println("Google Login");
+	}
+	@Test
+	public void webapps()
+	{
+		System.out.println("Facebook Login");
+	}
+}
